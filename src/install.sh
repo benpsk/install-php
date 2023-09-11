6bd002eec030cc192209fb0476a48fc9903bff75
Ben Gunn <bengunn.dev@gmail.com>
2023-09-12 04:35:35 +0630

    add mariadb

--- src/install.sh
+++ src/install.sh
@@ -21,8 +21,23 @@ if dpkg -l | grep -q mariadb-server; then
 	echo "Remove Successfuly"
 else
 	echo "MariaDB is not installed."
-	# Perform actions when MariaDB is not installed
-	# For example, install MariaDB:
-	# sudo apt-get update
-	# sudo apt-get install mariadb-server
+
+	sudo apt-get install apt-transport-https curl
+	sudo mkdir -p /etc/apt/keyrings
+	sudo curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+	sudo echo "# MariaDB 11.1 repository list - created 2023-09-11 22:00 UTC
+  # https://mariadb.org/download/
+  X-Repolib-Name: MariaDB
+  Types: deb
+  # deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+  # URIs: https://deb.mariadb.org/11.1/ubuntu
+  URIs: https://mirror.kku.ac.th/mariadb/repo/11.1/ubuntu
+  Suites: jammy
+  Components: main main/debug
+  Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+  " >/etc/apt/sources.list.d/mariadb.sources
+
+	sudo apt-get update
+	sudo apt-get install mariadb-server
+
 fi
