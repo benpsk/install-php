d1bcd18ee39a732697c903cd4a51b548e1d116c4
Ben Gunn <bengunn.dev@gmail.com>
2023-09-12 12:45:55 +0630

    extract php script

--- src/install.sh
+++ src/install.sh
@@ -5,6 +5,24 @@ set -e
 
 #### Disclaimer ####
 echo "Please keep in mind that if there's the exiting mariadb it'll backup and install new!"
+echo ""
+echo ""
+
+## install php (ondrje php)
+
+### add ondrje ppa
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update
+
+### install php8.2
+sudo apt install -y php8.2
+
+### install php modules (Laravel Project)
+sudo apt install -y php8.2-mysql php8.2-mbstring php8.2-exif php8.2-bcmath php8.2-gd php8.2-zip php8.2-dom
+
+#### Disclaimer ####
+echo "Please keep in mind that if there's the exiting mariadb it'll backup and install new!"
 
 #### start backup if exist ####
 
@@ -19,13 +37,12 @@ if dpkg -l | grep -q mariadb-server; then
 	sudo apt autoremove -y
 
 	echo "Remove Successfuly"
-else
-	echo "MariaDB is not installed."
+fi
 
-	sudo apt-get install apt-transport-https curl -y
-	sudo mkdir -p /etc/apt/keyrings
-	sudo curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
-	sudo sh -c 'echo "# MariaDB 11.1 repository list - created 2023-09-11 22:00 UTC
+sudo apt-get install apt-transport-https curl -y
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+sudo sh -c 'echo "# MariaDB 11.1 repository list - created 2023-09-11 22:00 UTC
   # https://mariadb.org/download/
   X-Repolib-Name: MariaDB
   Types: deb
@@ -34,10 +51,7 @@ else
   URIs: https://mirror.kku.ac.th/mariadb/repo/11.1/ubuntu
   Suites: jammy
   Components: main main/debug
-  Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
-  " >/etc/apt/sources.list.d/mariadb.sources'
+  Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp " >/etc/apt/sources.list.d/mariadb.sources'
 
-	sudo apt-get update -y
-	sudo apt-get install mariadb-server -y
-
-fi
+sudo apt-get update -y
+sudo apt-get install mariadb-server -y
---
+++ src/php.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# exit on error
+set -e
+
+#### Disclaimer ####
+echo "***********Disclaimer**********"
+echo ""
+echo "The existing PHP will be overwritten by the new PHP!"
+echo ""
+echo ""
+
+## install php (ondrje php)
+
+### add ondrje ppa
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update
+
+### install php8.2
+sudo apt install -y php8.2
+
+### install php modules (Laravel Project)
+sudo apt install -y php8.2-mysql php8.2-mbstring php8.2-exif php8.2-bcmath php8.2-gd php8.2-zip php8.2-dom
