eaa9fd6cc23ae2531f241424551ea845ba4954f3
benpsk <bengunn.dev@gmail.com>
2023-09-13 21:15:04 +0630

    update php promt #2

--- src/php.sh
+++ src/php.sh
@@ -10,8 +10,8 @@ echo
 VALID_PHP_VERSION=("8.2" "8.1" "8.0" "7.4" "7.2")
 
 ask_php_version() {
-	echo "Install PHP"
-	echo
+	echo "Available PHP Versions"
+	echo 
 	echo "1. PHP 8.2 (default)"
 	echo "2. PHP 8.1"
 	echo "3. PHP 8.0"
@@ -20,6 +20,7 @@ ask_php_version() {
 	echo "Enter 'q' to quit."
 	echo
 	read -p "Please select PHP Version: " php_version 
+	echo
 }
 
 # validate php version input
@@ -32,13 +33,9 @@ validate_php_version() {
 	if [[ "$input" =~ $pattern ]]; then
 		return 0
 	else
-		echo "Invalid PHP Version: $input"
 		return 1
 	fi
 }
-
-# Initialize PHP_VERSION
-php_version=""
 
 # Keep asking until a valid PHP version is provided
 while true; do
@@ -46,18 +43,23 @@ while true; do
 
 	php_version=$(echo $php_version | tr '[A-Z]' '[a-z]')
 
-	echo "php version "
-	echo "$php_version"
 	if [ "$php_version" = "q" ]; then
-		echo "exiting the script"
+		echo "Goodbye, See you next time!"
 		exit 0
 	fi
-
+	
+	if [ -z "$php_version" ]; then
+	    php_version="8.2"
+	fi
 
 	if [[ " ${VALID_PHP_VERSION[*]} " == *" ${php_version} "* ]]
        	then
 		echo "Valid PHP Version"
 		break
+	else 
+		echo "--------------------------------"
+		echo	
+		echo "Invalid PHP Version: $php_version"
+		echo 
 	fi
 done
-echo "Selected PHP Version: $php_version"
