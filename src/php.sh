5d7d339355ab18f07b5e83ae47ec70d8057eea88
benpsk <bengunn.dev@gmail.com>
2023-09-13 20:47:50 +0630

    update php prompt

--- src/php.sh
+++ src/php.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# exit on error
-set -e
-
 #### Disclaimer ####
 echo "***********Disclaimer**********"
 echo
 echo "The existing PHP will be overwritten by the new PHP!"
 echo
 echo
+
+VALID_PHP_VERSION=("8.2" "8.1" "8.0" "7.4" "7.2")
 
 ask_php_version() {
 	echo "Install PHP"
@@ -20,44 +19,45 @@ ask_php_version() {
 	echo "5. PHP 7.2"
 	echo "Enter 'q' to quit."
 	echo
-	read -p "Please select PHP Version: " PHP_VERSION
+	read -p "Please select PHP Version: " php_version 
 }
 
 # validate php version input
 validate_php_version() {
 	local input="$1"
-	ans=$(echo $input | tr '[A-Z]' '[a-z]')
-	if [ "$ans" == "q" ]; then
-		echo "YOu enter quite"
-		return 2
-	else
-		# Define a regular expression pattern for decimal numbers
-		local pattern='^[0-9]+([.][0-9]+)?$'
+	
+	# Define a regular expression pattern for decimal numbers
+	local pattern='^[0-9]+([.][0-9]+)?$'
 
-		if [[ "$input" =~ $pattern ]]; then
-			return 0
-		else
-			echo "Invalid PHP Version: $input"
-			return 1
-		fi
+	if [[ "$input" =~ $pattern ]]; then
+		return 0
+	else
+		echo "Invalid PHP Version: $input"
+		return 1
 	fi
 }
 
 # Initialize PHP_VERSION
-PHP_VERSION=""
+php_version=""
 
 # Keep asking until a valid PHP version is provided
 while true; do
 	ask_php_version
 
-	result=$(validate_php_version "$PHP_VERSION")
+	php_version=$(echo $php_version | tr '[A-Z]' '[a-z]')
 
-	if [ "$result" -eq 2 ]; then
-		echo "Exiting the script."
+	echo "php version "
+	echo "$php_version"
+	if [ "$php_version" = "q" ]; then
+		echo "exiting the script"
 		exit 0
-	elif [ "$result" -eq 0 ]; then
-		break # Exit the loop when a valid version is provided
+	fi
+
+
+	if [[ " ${VALID_PHP_VERSION[*]} " == *" ${php_version} "* ]]
+       	then
+		echo "Valid PHP Version"
+		break
 	fi
 done
-
-echo "Selected PHP Version: $PHP_VERSION"
+echo "Selected PHP Version: $php_version"
