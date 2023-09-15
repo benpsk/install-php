35a1aaecdc07f8d2d6e6b57385d4fc4327dec400
Ben Gunn <bengunn.dev@gmail.com>
2023-09-15 19:27:53 +0630

    update iMagic

--- iMagic.sh
+++ iMagic.sh
@@ -28,7 +28,7 @@ backup_and_remove_apache2() {
 ##
 ##
 ##
-## *************** END DATABASE SECTION ****************
+## *************** END COMMON FUNCTION SECTION ****************
 ##
 ##
 ##
@@ -230,7 +230,7 @@ if [ "$composer" = "y" ] && [ "$COMPOSER_SKIP" = "false" ]; then
 	ehco "Cleaning Composer..."
 	sudo apt-get remove composer -y
 
-	echo "Installing composer..."
+	echo "Installing Composer..."
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	php composer-setup.php
