8b564982f974c0940daa9304fd31565238a0bb7c
Ben Gunn <bengunn.dev@gmail.com>
2024-04-04 00:43:57 +0630

    issue: fix compoer install failure

--- iMagic.sh
+++ iMagic.sh
@@ -400,11 +400,22 @@ if [ "$composer" = "y" ] && [ "$COMPOSER_SKIP" = "false" ]; then
 
 	install_message "Composer"
 
+	EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-	php composer-setup.php
-	php -r "unlink('composer-setup.php');"
+	ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+	if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+	then
+ 		>&2 echo 'ERROR: Invalid installer checksum'
+  	  	rm composer-setup.php
+   	 	exit 1
+	fi
+
+	php composer-setup.php --quiet
+	RESULT=$?
+	rm composer-setup.php
 	sudo mv composer.phar /usr/local/bin/composer
+	exit $RESULT
 fi
 
 ##
